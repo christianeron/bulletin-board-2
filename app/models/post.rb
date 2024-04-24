c9732c4388e282abc77f7bc50eebf5b2c90d98a3
christianeron <christianeron1@gmail.com>
2024-04-24 21:28:34 +0000

    full grade

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,6 +12,8 @@
 #  user_id    :integer
 #
 class Post < ApplicationRecord
+  belongs_to(:user)
+  belongs_to(:board)
   validates(:title, presence: true)
   validates(:body, presence: true)
   validates(:expires_on, presence: true)
